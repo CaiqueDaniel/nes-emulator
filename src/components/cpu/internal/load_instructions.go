d6bb00b18c2cc9707a14b9a5894efd2994b5f58e
¨Caique <caiquedaniel@outlook.com.br>
2026-06-08 23:46:08 -0300

    refactor: remove redundant CPU load and store instruction tests

--- src/components/cpu/internal/load_instructions.go
+++ src/components/cpu/internal/load_instructions.go
@@ -10,30 +10,3 @@ func (c *cpu) LoadValueIntoRegister(value uint8, register string) {
 		c.acc = value
 	}
 }
-
-func (c *cpu) LoadValueFromMemoryIntoRegister(address uint16, register string) {
-	value := c.memory.Read(address)
-	c.LoadValueIntoRegister(value, register)
-}
-
-func (c *cpu) LoadValueFromMemoryIntoRegisterZeroPage(address uint8, register string) {
-	c.LoadValueFromMemoryIntoRegister(uint16(address), register)
-}
-
-func (c *cpu) LoadValueFromMemoryIntoAccumulatorWithIndexAbsolute(address uint16, register string) {
-	value := c.memory.Read(c.GetAddressByIndexedAbsoluteMode(address, register))
-	c.acc = value
-}
-
-func (c *cpu) LoadValueFromMemoryIntoAccumulatorWithIndexZeroPage(address uint8, register string) {
-	value := c.memory.Read(c.GetAddressByIndexedZeroPageMode(address, register))
-	c.acc = value
-}
-
-func (c *cpu) LoadValueFromMemoryIntoRegisterWithIndexedXIndirectAddress(initialAddress uint8, register string) {
-	c.LoadValueIntoRegister(c.memory.Read(c.GetAddressByIndexedIndirectXMode(initialAddress)), register)
-}
-
-func (c *cpu) LoadValueFromMemoryIntoRegisterWithIndirectIndexedYAddress(initialAddress uint8, register string) {
-	c.LoadValueIntoRegister(c.memory.Read(c.GetAddressByIndirectIndexedYMode(initialAddress)), register)
-}
--- src/components/cpu/internal/store_instructions.go
+++ src/components/cpu/internal/store_instructions.go
@@ -10,38 +10,3 @@ func (c *cpu) StoreRegisterIntoAbsoluteMemory(value uint16, register string) {
 		c.memory.Write(value, c.acc)
 	}
 }
-
-func (c *cpu) StoreRegisterIntoZeroPageMemory(value uint8, register string) {
-	c.StoreRegisterIntoAbsoluteMemory(uint16(value), register)
-}
-
-func (c *cpu) StoreAccumulatorIntoMemoryWithIndexAbsolute(address uint16, register string) {
-	c.memory.Write(c.GetAddressByIndexedAbsoluteMode(address, register), c.acc)
-}
-
-func (c *cpu) StoreAccumulatorIntoMemoryWithIndexZeroPage(address uint8, register string) {
-	c.memory.Write(c.GetAddressByIndexedZeroPageMode(address, register), c.acc)
-}
-
-func (c *cpu) StoreRegisterIntoMemoryWithIndirectAddress(initialAddress uint16, register string) {
-	c.storeRegisterIntoMemory(c.GetAddressByIndirectAbsoluteMode(initialAddress), register)
-}
-
-func (c *cpu) StoreRegisterIntoMemoryWithIndexedXIndirectAddress(initialAddress uint8, register string) {
-	c.storeRegisterIntoMemory(c.GetAddressByIndexedIndirectXMode(initialAddress), register)
-}
-
-func (c *cpu) StoreRegisterIntoMemoryWithIndirectIndexedYAddress(initialAddress uint8, register string) {
-	c.storeRegisterIntoMemory(c.GetAddressByIndirectIndexedYMode(initialAddress), register)
-}
-
-func (c *cpu) storeRegisterIntoMemory(address uint16, register string) {
-	switch register {
-	case REGISTER_X:
-		c.memory.Write(address, c.x)
-	case REGISTER_Y:
-		c.memory.Write(address, c.y)
-	case ACCUMULATOR:
-		c.memory.Write(address, c.acc)
-	}
-}
