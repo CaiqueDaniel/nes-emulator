855fa746c828763860e5369a4c1f5d6c9757f8c1
¨Caique <caiquedaniel@outlook.com.br>
2026-06-08 23:42:28 -0300

    feat: implement indirect addressing mode value retrieval methods and add corresponding unit tests

--- src/components/cpu/internal/memory_addressing_mode.go
+++ src/components/cpu/internal/memory_addressing_mode.go
@@ -16,6 +16,27 @@ func (c *cpu) GetValueByZeroPageIndexedMode(address uint8, index uint8) uint8 {
 	return c.memory.Read(uint16(address) + uint16(index))
 }
 
+func (c *cpu) GetValueByIndirectAbsoluteMode(initialAddress uint16) uint8 {
+	lastByte := c.memory.Read(initialAddress)
+	firstByte := c.memory.Read(initialAddress + 1)
+
+	return c.memory.Read(uint16(firstByte)<<8 | uint16(lastByte))
+}
+
+func (c *cpu) GetValueByIndexedIndirectXMode(initialAddress uint8) uint8 {
+	pivotAddress := uint16(initialAddress) + uint16(c.x)
+	lastByte := c.memory.Read(pivotAddress)
+	firstByte := c.memory.Read(pivotAddress + 1)
+
+	return c.memory.Read(uint16(firstByte)<<8 | uint16(lastByte))
+}
+
+func (c *cpu) GetValueByIndirectIndexedYMode(initialAddress uint8) uint8 {
+	lastByte := c.memory.Read(uint16(initialAddress))
+	firstByte := c.memory.Read(uint16(initialAddress) + 1)
+	return c.memory.Read(uint16(firstByte)<<8 | uint16(lastByte) + uint16(c.y))
+}
+
 // Remover
 func (c *cpu) GetAddressByIndexedAbsoluteMode(address uint16, register string) uint16 {
 	var registerValue uint8
@@ -45,12 +66,14 @@ func (c *cpu) GetAddressByIndexedZeroPageMode(address uint8, register string) ui
 	return uint16(finalAddress)
 }
 
+// Remover
 func (c *cpu) GetAddressByIndirectAbsoluteMode(initialAddress uint16) uint16 {
 	lastByte := c.memory.Read(initialAddress)
 	firstByte := c.memory.Read(initialAddress + 1)
 	return uint16(firstByte)<<8 | uint16(lastByte)
 }
 
+// remover
 func (c *cpu) GetAddressByIndexedIndirectXMode(initialAddress uint8) uint16 {
 	pivotAddress := uint16(initialAddress) + uint16(c.x)
 	lastByte := c.memory.Read(pivotAddress)
@@ -58,6 +81,7 @@ func (c *cpu) GetAddressByIndexedIndirectXMode(initialAddress uint8) uint16 {
 	return uint16(firstByte)<<8 | uint16(lastByte)
 }
 
+// remover
 func (c *cpu) GetAddressByIndirectIndexedYMode(initialAddress uint8) uint16 {
 	lastByte := c.memory.Read(uint16(initialAddress))
 	firstByte := c.memory.Read(uint16(initialAddress) + 1)
--- src/components/cpu/internal/tests/memory_addressing_mode_test.go
+++ src/components/cpu/internal/tests/memory_addressing_mode_test.go
@@ -58,3 +58,50 @@ func TestGetValueByZeroPageIndexedMode(t *testing.T) {
 		t.Errorf("Expected %d, got %d", 0xFF, value)
 	}
 }
+
+func TestGetValueByIndirectAbsoluteMode(t *testing.T) {
+	memory := memory.NewMemory()
+	cpu := internal.NewCpu(memory)
+
+	memory.Write(0xFAFF, 0x80)
+	memory.Write(0x1000, 0xFF)
+	memory.Write(0x1001, 0xFA)
+
+	value := cpu.GetValueByIndirectAbsoluteMode(0x1000)
+
+	if value != 0x80 {
+		t.Errorf("Expected %d, got %d", 0x80, value)
+	}
+}
+
+func TestGetValueByIndexedIndirectXMode(t *testing.T) {
+	memory := memory.NewMemory()
+	cpu := internal.NewCpu(memory)
+
+	memory.Write(0xFAFF, 0x80)
+	memory.Write(0x01, 0xFF)
+	memory.Write(0x02, 0xFA)
+	cpu.LoadValueIntoRegister(0x1, internal.REGISTER_X)
+
+	value := cpu.GetValueByIndexedIndirectXMode(0x0)
+
+	if value != 0x80 {
+		t.Errorf("Expected %d, got %d", 0x80, value)
+	}
+}
+
+func TestGetValueByIndirectIndexedYMode(t *testing.T) {
+	memory := memory.NewMemory()
+	cpu := internal.NewCpu(memory)
+
+	memory.Write(0xFAFF, 0x80)
+	memory.Write(0x01, 0xFF)
+	memory.Write(0x02, 0xFA)
+	cpu.LoadValueIntoRegister(0x1, internal.REGISTER_X)
+
+	value := cpu.GetValueByIndexedIndirectXMode(0x0)
+
+	if value != 0x80 {
+		t.Errorf("Expected %d, got %d", 0x80, value)
+	}
+}
