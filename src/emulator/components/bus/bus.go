78127f6c329b3d36bb14c0ec48eaac40d1222798
¨Caique <caiquedaniel@outlook.com.br>
2026-06-22 23:43:07 -0300

    feat: implement bus cycle tracking by injecting bus dependency into memory and triggering ticks on read/write operations

--- src/emulator/components/bus/bus.go
+++ src/emulator/components/bus/bus.go
@@ -6,11 +6,17 @@ import (
 
 type bus struct {
 	tickables []application.Tickable
+	tickCount uint
 }
 
 func NewBus() application.Bus {
+	return NewBusWithInternalType()
+}
+
+func NewBusWithInternalType() *bus {
 	return &bus{
 		tickables: make([]application.Tickable, 0),
+		tickCount: 0,
 	}
 }
 
@@ -22,4 +28,9 @@ func (b *bus) Tick(cycles int) {
 	for _, tickable := range b.tickables {
 		tickable.Tick()
 	}
+	b.tickCount++
+}
+
+func (b *bus) GetTickCount() uint {
+	return b.tickCount
 }
--- src/emulator/components/memory/memory.go
+++ src/emulator/components/memory/memory.go
@@ -5,19 +5,23 @@ import (
 )
 
 type memory struct {
+	bus     application.Bus
 	storage map[uint16]uint8
 }
 
-func NewMemory() application.Memory {
+func NewMemory(bus application.Bus) application.Memory {
 	return &memory{
+		bus:     bus,
 		storage: make(map[uint16]uint8),
 	}
 }
 
 func (mc *memory) Read(address uint16) uint8 {
+	mc.bus.Tick(1)
 	return mc.storage[address]
 }
 
 func (mc *memory) Write(address uint16, value uint8) {
+	mc.bus.Tick(1)
 	mc.storage[address] = value
 }
