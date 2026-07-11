b98cad0924e2aa6f3c88fcf38f9123f6889d70f7
¨Caique <caiquedaniel@outlook.com.br>
2026-07-11 16:09:28 -0300

    refactor: remove redundant StoreRegisterIntoAbsoluteMemory helper function

--- src/emulator/components/cpu/access_instructions.go
+++ src/emulator/components/cpu/access_instructions.go
@@ -1,5 +1,16 @@
 package cpu
 
+func (c *cpu) StoreRegisterIntoAbsoluteMemory(value uint16, register string) {
+	switch register {
+	case REGISTER_X:
+		c.memory.Write(value, c.x)
+	case REGISTER_Y:
+		c.memory.Write(value, c.y)
+	case ACCUMULATOR:
+		c.memory.Write(value, c.acc)
+	}
+}
+
 func (c *cpu) LoadValueIntoRegister(value uint8, register string) {
 	switch register {
 	case REGISTER_X:
