1199634e5ae13ade1ba547452be8b06db62ad80a
Caique Daniel <caiquedaniel@outlook.com.br>
2026-09-13 00:34:18 -0300

    feat: add PPUController

---
+++ src/emulator/ppu/delivery/ppu_controller.go
@@ -0,0 +1,31 @@
+package delivery
+
+import "nes-emu/src/emulator/ppu/application"
+
+type PPUController struct {
+	initialized    bool
+	renderGraphics *application.RenderGraphics
+}
+
+type RenderRequest application.RenderGraphicsInput
+
+func NewPPUController(renderGraphics *application.RenderGraphics) *PPUController {
+	return &PPUController{
+		initialized:    true,
+		renderGraphics: renderGraphics,
+	}
+}
+
+func (p *PPUController) Render(request *RenderRequest) {
+	p.checkIfInitilized()
+	p.renderGraphics.Execute(&application.RenderGraphicsInput{
+		Address: request.Address,
+		IsWrite: request.IsWrite,
+	})
+}
+
+func (p *PPUController) checkIfInitilized() {
+	if !p.initialized {
+		panic("PPUController was not initilized")
+	}
+}
