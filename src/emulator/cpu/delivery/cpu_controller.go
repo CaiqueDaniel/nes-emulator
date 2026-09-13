e906fe7582d3bec1bad19b9a1d6c58987471469d
Caique Daniel <caiquedaniel@outlook.com.br>
2026-09-13 02:59:12 -0300

    feat: add cpu controller

---
+++ src/emulator/cpu/delivery/cpu_controller.go
@@ -0,0 +1,26 @@
+package delivery
+
+import "nes-emu/src/emulator/cpu/internal/application"
+
+type CPUController struct {
+	initialize bool
+	cpu        *application.CPU
+}
+
+func NewCPUController(cpu *application.CPU) *CPUController {
+	return &CPUController{
+		initialize: true,
+		cpu:        cpu,
+	}
+}
+
+func (c *CPUController) RunProgram() {
+	c.checkIfInitialize()
+	c.cpu.RunProgram()
+}
+
+func (c *CPUController) checkIfInitialize() {
+	if !c.initialize {
+		panic("CPU not initialized")
+	}
+}
